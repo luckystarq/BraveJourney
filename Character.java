aa23c99e59acf9f8650b65a0e984bfb19e1c2d9e
luckystarq <az8395831@qq.com>
2025-09-09 11:03:30 +0800

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,1 +1,141 @@
+包模型；
 
+/**
+* 角色类，统一表示游戏中的所有角色（玩家和敌人）
+* 包含基本属性和战斗相关方法
+ */
+公共 班级 性格 {
+    // 角色类型枚举
+    公共 enum 类型 {
+        运动员,  // 玩家角色
+敌人// 敌人角色
+    }
+    
+    // 角色唯一标识
+私人的 最终的 线 身份标识;
+    // 角色名称
+私人的 最终的 线 姓名;
+    // 当前生命值
+私人的int currentHealth;
+    // 最大生命值
+私人的 最终的int MaxHealth;
+    // 攻击力
+私人的int攻击;
+    // 防御力
+私人的int防卫;
+    // 角色类型（玩家/敌人）
+私人的 最终的 类型 类型;
+    
+    /**
+* 玩家角色构造函数
+*@param id 唯一标识
+*@param name 名称
+*@param MaxHealth 最大生命值
+*@param 攻击攻击力
+*@param 防御防御力
+     */
+    公共 性格(线 身份标识, 线 姓名, int MaxHealth, int 攻击, int 防卫) {
+        这(ID，姓名，MaxHealth，攻击，防御，类型.播放器);
+    }
+    
+    /**
+     * 通用角色构造函数
+     * @param id 唯一标识
+     * @param name 名称
+     * @param maxHealth 最大生命值
+     * @param attack 攻击力
+*@param 防御防御力
+*@param 类型角色类型
+     */
+    公共 性格(线 身份标识, 线 姓名, int MaxHealth, int 攻击, int 防卫, 类型 类型) {
+        这.id=id；
+        这.name=名称；
+        这.maxHealth=MaxHealth；
+        这.currentHealth=MaxHealth；
+        这.ttack=攻击；
+        这defense=defense；
+        这.type=类型；
+    }
+    
+    /**
+* 角色受到伤害
+*@param 损坏伤害值
+     */
+    公共 无效的 takeDamage(int 损害) {
+        // 计算实际伤害（考虑防御力）
+        int 实际损坏=数学。最大值(1，损坏-防御);
+currentHealth=数学。最大值(0，当前健康-实际损坏);
+system.out.println(姓名+"受到了"+实际损坏+"点伤害！");
+    }
+    
+    /**
+     * 角色治疗
+     * @param amount 治疗量
+     */
+    public void heal(int amount) {
+        if (amount <= 0) return;
+        currentHealth = Math.min(maxHealth, currentHealth + amount);
+    }
+    
+    /**
+     * 攻击目标角色
+     * @param target 被攻击的目标
+     */
+    public void attack(Character target) {
+        System.out.println(name + "攻击了" + target.name + "！");
+        target.takeDamage(attack);
+    }
+    
+    /**
+     * 提升角色属性
+     * @param attackBonus 攻击力加成
+*@param defenseBonus 防御力加成
+     */
+公共虚空提升统计(int攻击加成，int防御加成){公共无效的boostStats(内部攻击相加，内部防御相加，int defenseBonus){boostStats(int attackBonus，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(内部攻击加成，内部防御加成){无效的boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(int attackBonus，int defenseBonus){boostStats(int attackBonus，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(int attackBonus，int defenseBonus){boostStats(int attackBonus，防御相加){(int攻击相加，我无效的boostStats(内部攻击相加的boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(加成，内部防御加成){boostStats(内部攻击加成，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(内部攻击加成，内部防御加成){无效的boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击加成，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击加成，防御相加){(int攻击相加，我无效的boostStats(内部攻击相加的boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(内部攻击加成，内部){boostStats(内部攻击加成，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击加成，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(intattackBonus，intdefenseBonus){){(int攻击相加，int防御相加){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击加成，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击加成，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(intattackBonus防御加成){boostStats(内部攻击加成，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击加成，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(intattackBonus，intdefenseBonus){){(int攻击相加，int防御相加){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击加成，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击加成，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(intattackBo){boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(int attackBonus，int defenseBonus){boostStats(int attackBonus，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(intattackBonus，intdefenseBonus){ 无效的boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(int attackBonus，int defenseBonus){boostStats(int attackBonus，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击加成，内部防御加成){(int攻击加成，int防御加成){公共无效的boostStats(int attackBonus，int defenseBonus){boostStats(NUS，intdenseBonus){boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击加成，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(内部攻击加成，内部防御加成){无效的boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击加成，防御加分){(int攻击加成，int防御加成){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击相加，内部防御相加){(int攻击相加，int防御相加){公共无效的boostStats(内部攻击加成，内部防御加成){boostStats(内部攻击加成){(int攻击加成，int防御加成){公共 无效的boostStats(intattackBonus，intdefenseBonus) {
+这个这个。ttack+=attackBonus；this.ttack+=attackBonus；this.攻击+=attackBonus；this。tttack+=attackBonus；this。攻击+=attackBonus；this。tttack+=attackBonus；this。攻击+=attackBonus；this。tttack+=attackBonus；this。攻击+=attackBonus；this。tttack+=attackBonus；this。攻击+=attackBonus；this。tttack+=attackBonus；this。攻击+=attackBonus；this。tttack+=attackBonus；这个。攻击+=attackBonus；this。tttack+=attackBonus；这个。攻击+=attackBonus；
+防御+=防御奖金；这。防御+=防御奖金；辩护奖金；这.辩护+=辩护奖金；
+if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(if(attackBonus>0)))){if(if(if(if(attackBonus>0)){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){如果(attackBonus>0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0)){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0))){if(atta0)))){if(if(if(if(attackBonus>0)){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0)){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0))){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){如果(attackBonus>0){0)){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0)){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){if(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果(attackBonus>0){0){如果 (attackBonus>0) {
+system.out.
+}}
+如果0){if(防御奖励>0){0){if(0){if(防御奖励>0){if(防御奖励>0)(如果(如果(如果(0){if(defenseBonus>0){0){if(defenseBonus>0){0){if(defenseBonus>0){如果(如果(如果((防御奖励>0))){如果(防御奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖金>0){0){if(防御奖金>0){0){if(防御奖金>0){0){if(防御奖金>0){0){if(防御奖金>0){0){if(防御奖金>0){0){if(防御奖金>0){0){if(defenseBonus>0){(如果(如果(如果(如果(如果(如果(defensedefenseBonus>0))){如果(defensed奖励>0){0){if(防御奖励>0){0){if(防御奖励>0){0){if(防御奖励>0){0){if(如果(如果(如果((防御奖励>0))){如果(防御奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖励奖金>0){0){if(防御奖金>0){0){if(防御奖金>0){0){if(防御奖金>0){0){if(防御奖金>0){0){if(防御奖金>0){0){if(防御奖金>0){0){if(防御奖金>0){0){if(defenseBonus>0){(如果(如果(如果(如果(defensedefenseBonus>0)))){如果(defensed奖励>0){0){if(防御奖励>0){0){if(防御奖励>0){0){if(防御奖励>0){0){if(防御奖励>0){0){if(防御奖励>0){0){if(防御奖励>0){0){if(防御奖励>0){if(防御奖励>0){if(防御奖励>0)if(防御奖励>0){0){if(防御0){0){if(防御奖励>0){0){if(防御奖励>0){if(防御奖励>0)
+system.out.
+}}
+}}
+    
+/**//**/**
+* 检查角色是否存活
+*@UE，否则返回假的 返回存活返回正确，否则返回假的 返回存活返回正确，否则返回假的
+*/
+公共 布尔型
+返回currentHealth>
+}}
+    
+//Getter和setter方法//吸气剂和setter方法
+公共 线
+返回身份标识；
+}}
+    
+公共 线
+返回姓名；
+}}
+    
+公共
+返回currentHealth；
+}}
+    
+公共
+返回MaxHealth；
+}}
+    
+公共
+返回攻击；
+}}
+    
+公共
+返回防卫；
+}}
+    
+公共类型*@param 防御防御力getTypegetTypegetType){getType() {
+返回类型；
+    }
+  }
