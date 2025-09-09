51a21fdf45781fc3a54fb80fd9114f06a3e6c65b
luckystarq <az8395831@qq.com>
2025-09-09 11:17:41 +0800

    Update GameManager.java

--- GameManager.java
+++ GameManager.java
@@ -1,1 +1,205 @@
+import model.Character;
+import model.LevelMap;
+import model.Node;
+import model.Event;
+import model.Level;
+import model.Combat;
+import model.Item;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+/**
+ * 游戏管理器类，负责统筹整个游戏流程
+ * 包括地图生成、节点选择、战斗和事件处理等核心逻辑
+ */
+public贝特。 开始class GameManager {
+    // 当前玩家角色实例
+    private final Character player;
+    // 当前地图实例
+    private LevelMap currentMap;
+    // 当前选中的节点
+    private Node currentNode;
+    // 随机数生成器，用于地图生成和随机事件
+    private final Random random = new Random();
+    
+    /**
+     * 构造函数
+     * @param player 玩家角色实例
+     */
+    public GameManager(Character player) {
+        this.player = player;
+    }
+    
+    /**
+     * 生成新的关卡地图
+     * @param id 地图唯一标识
+     * @param name 地图名称
+     * @param nodeCount 节点数量
+     */
+    public void generateMap(String id, String name, int nodeCount) {
+        List<Node> nodes = new ArrayList<>();
+        
+        // 创建起始节点（休息类型）
+        Node startNode = createNode("node_0", Node.Type.REST, "起点", 
+                                   "开始冒险的地方", "", getConnectedNodes(0，nodeCount));
+节点。添加(startnode);
+        
+        // 创建其他节点
+        为 (int 我=1；i<nodeCount；i++) {
+            节点.类型 类型=getRandomNodeType();
+            线 relatedId="内容_"+i；
+            节点 节点=CreateNode("node_"+i，类型，getNodeName(类型，i), 
+                                 getNodeDescription(类型)，relatedId，
+                                 getConnectedNodes(I，nodeCount));
+节点。添加(节点);
+        }
+        
+currentMap=新的 LevelMap(ID、名称、节点、开始节点。getId());
+currentMap.unlockStartingNode();
+currentNode=startnode；
+    }
+    
+    /**
+* 选择地图上的节点
+*@param nodeId 要选择的节点ID
+*@return 选择成功返回true，否则返回false
+     */
+    公共 布尔型 selectNode(线 nodeID) {
+        节点 节点=currentMap。GetNode(nodeID);
+        如果 (node==无效的||！节点。isunlocked()||节点。isCompleted()) {
+            返回 假的;
+        }
+        
+        // 标记当前节点为已完成
+        如果 (currentNode！=无效的) {
+            currentNode.markCompleted();
+        }
+        
+        // 更新当前节点
+        currentNode = node;
+        return true;
+    }
+    
+    /**
+     * 进入当前选中的节点，处理节点内容
+     */
+    public void enterCurrentNode() {
+        if (currentNode == null) return;
+        
+        switch (currentNode.getType()) {
+            case BATTLE -> handleBattleNode();
+            case EVENT -> handleEventNode();
+            case REST -> handleRestNode();
+        }
+        
+        // 解锁连接的节点
+        currentMap.unlockConnectedNodes(currentNode.getId());
+    }
+    
+    /**
+* 处理战斗节点逻辑
+     */
+私有无效的handleBattleNode(){handleBattleNode(){handleBattleNode(){handleBattleNode(){handleBattleNode(){handleBattleNode(){handleBattleNode(){handleBattleNode(){
+水平水平=createSampleLevel(当前节点。getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(当前节点。getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(当前节点。getRelatedContentId())；createSampleLevel(currentNode。getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(createSampleLevel(当前节点.getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；创建())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(当前节点。getRelatedContentId())；createSampleLevel(currentNode。getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(createSampleLevel(当前节点。getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(currentNode。getRelatedContentId())；createSenplateLevel(currentNode.getRelatedContentId())；createSampleLevel(当前节点。getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(currentNode。getRelatedContentId())；createSampleLevel(currentNode。getRelatedContentId())；getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(currentNode。getRelatedContentId())；createSampleLevel(currentNode。getRelatedContentId())；createSampleLevel(当前节点.getRelatedContentId())；createSampleLevel(currentNode.得到(currentNode。getRelatedContentId())；createSampleLevel(当前节点。getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(currentNode。getRelatedContentId())；createSampleLevel(currentNode。getRelatedContentId())；getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；createSampleLevel(currentNode。getRelatedContentId())；createSampleLevel(currentNode。getRelatedContentId())；createSampleLevel(当前节点。getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())RelatedContentId())；createSampleLevel(当前节点。getRelatedContentId())；createSampleLevel(currentNode.getRelatedContentId())；(currentNode。getRelatedContentId())；createSampleLevel(currentNode。getRelatedContentId())；
+战斗战斗=新战斗(玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡); (玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡);(玩家，等级)；(玩家，关卡);
+贝特。开始()；()；()；()；()；()；()；()；()；()；()；()；()；()；()；()；
+        
+如果(战役。 是胜利的()){(战斗.是胜利的()){(战役。 是胜利的()){(战斗.是胜利的()){(战役。 是胜利的()){(战斗.是胜利的()){(战役。 是胜利的()){(战斗.是胜利的()){
+战斗.grantRewards()；grantRewards()；grantRewards()；grantRewards()；grantRewards()；grantRewards()；grantRewards()；grantRewards()；
+}
+}
+    
+/**
+* 处理事件节点逻辑
+     */
+私有无效的handleEventNode(){handleEventNode(){handleEventNode(){handleEventNode(){handleEventNode(){handleEventNode(){handleEventNode(){handleEventNode(){handleEventNode(){handleEventNode(){
+事件事件=createSampleEvent(当前节点。getRelatedContentId())；createSampleEvent(currentNode.getRelatedContentId())；createSampleEvent(当前节点。getRelatedContentId())；createSampleEvent(currentNode。getRelatedContentId())；createSampleEvent(当前节点。getRelatedContentId())；createSampleEvent(currentNode。getRelatedContentId())；createSampleEvent(当前节点。getRelatedContentId())；createSampleEvent(currentNode.getRelatedContentId())；ntentId())；createSampleEvent(当前节点。getRelatedContentId())；createSampleEvent(currentNode.getRelatedContentId())；createSampleEvent(当前节点。getRelatedContentId())；createSampleEvent(currentNode.getRelatedContentId())；
+// 简化处理：默认选择第一个选项// 简化处理：默认选择第一个选项
+事件。executeOption(0，播放器)；(0，播放器)；(0，播放器)；(0，播放器)；
+}
+    
+/**
+* 处理休息节点逻辑
+     */
+专用无效的(){handleRestNode(){(){handleRestNode(){handleRestNode(){handleRestNode(){handleRestNode(){(){handleRestNode(){handleRestNode(){
+intintint healAmount=播放器。getMaxHealth()/3；int healAmount=播放器。getMaxHealth()/3；healAmount=播放器。getMaxHealth()/3；int healAmount=播放器。getMaxHealth()/3；healAmount=播放器。getMaxHealth()/3；int healAmount=播放器。getMaxHealth()/3；healAmount=播放器。getMaxHealth()/3；int healAmount=播放器。getMaxHealth()/3；
+玩家.治愈(healAmount)；(healAmount)；(healAmount)；(healAmount)；(healAmount)；(healAmount)；(healAmount)；(healAmount)；(healAmount)；
+System.out.println("休息恢复了"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；println("休息"+healAmount+"点生命值")；println("休息恢复了"+healAmount+"点生命值")；
+}
+    
+// 辅助方法：创建节点// 辅助方法：创建节点
+专用节点CreateNode(字符串身份标识，节点。类型类型，字符串名称，字符串描述，CreateNode(字符串身份标识，节点。类型类型，字符串名称，字符串描述，CreateNode(线身份标识，节点.类型类型，线姓名，线DESC，CreateNode(字符串身份标识，节点。类型类型，字符串名称，字符串描述，CreateNode(字符串身份标识，节点。类型类型，字符串名称，字符串描述，CreateNode(线身份标识，节点.类型类型，线姓名，线DESC，CreateNode(字符串身份标识，节点。类型类型，字符串名称，字符串描述，CreateNode(字符串身份标识，节点。类型类型，字符串名称，字符串描述，CreateNode(线身份标识，节点.类型类型，线姓名，线DESC，CreateNode(字符串身份标识，节点。类型类型，字符串名称，字符串描述，CreateNode(字符串身份标识，节点。类型类型，字符串名称，字符串描述，CreateNode(线身份标识，节点.类型类型，线姓名，线DESC，
+线relatedId，列表<线>connectedNodes){){
+返回新的节点(身份标识、类型、名称、(身份标识、类型、名称、);(身份标识、类型、名称、(身份标识、类型、名称、);
+}}
+ 
+// 辅助方法：获取随机节点类型// 辅助方法：获取随机节点类型
+专用节点.类型getRandomNodeType(){私人的节点。类型getRandomNodeType(){getRandomNodeType() {
+节点.类型
+返回类型
+}}
+    
+// 辅助方法：生成节点名称// 辅助方法：生成节点名称
+私有字符串getNodeName
+返回开关
+案例战斗->
+案例事件->
+案例休息->“休息点”+索引；案例休息->
+};};
+}}
+    
+// 辅助方法：生成节点描述// 辅助方法：生成节点描述
+专用字符串
+返回开关
+案例战斗->
+案例事件->
+案例 休息->
+};};
+}}
+    
+//辅助方法：获取连接的节点ID列表//辅助方法：获取连接的节点ID列表
+个人分发列表<String>
+列表<字符串>已连接=新的ArrayList<>
+如果
+连接。添加
+}}
+如果(currentIndex>0随机(&&R).nextBoolean()){如果(currentIndex>0随机(&&R).nextBoolean()){
+连接。添加("node_"+(currentIndex-1))；添加("node_"+(currentIndex-1))；("node_"+(currentIndex-1))；添加("node_"+(currentIndex-1))；("node_"+(currentIndex-1))；添加("node_"+(currentIndex-1))；("node_"+(currentIndex-1))；添加("node_"+(currentIndex-1))；("node_"+(currentIndex-1))；添加("node_"+(currentIndex-1))；("node_"+(currentIndex-1))；添加("node_"+(currentIndex-1))；("node_"+(currentIndex-1))；添加("node_"+(currentIndex-1))；("node_"+(currentIndex-1))；添加("node_"+(currentIndex-1))；("node_"+(currentIndex-1))；添加("node_"+(currentIndex-1))；("node_"+(currentIndex-1))；添加("node_"+(currentIndex-1))；("node_"+(currentIndex-1))；添加("node_"+(currentIndex-1))；("node_"+(currentIndex-1))；添加("node_"+(currentIndex-1))；
+}}
+回路连接；返回连接；
+}}
+    
+// 辅助方法：创建示例关卡// 辅助方法：创建示例关卡
+专用级别createSampleLevel(字符串身份标识){私人的水平createSampleLevel(线身份标识){createSampleLevel(字符串ID){私人的水平createSampleLevel(线身份标识){createSampleLevel(字符串ID){私人的水平createSampleLevel(线身份标识){createSampleLevel(字符串ID){私人的水平createSampleLevel(线身份标识){(字符串身份标识){私人的水平createSampleLevel(线身份标识){createSampleLevel(字符串ID){私人的水平createSampleLevel(线身份标识){createSampleLevel(字符串ID){私人的水平createSampleLevel(线身份标识){createSampleLevel(字符串ID){私人的水平createSampleLevel(线身份标识){
+列表<字符>敌人=新ArrayList<>()；列表<性格>敌人=新的ArrayList<>()；ArrayList<>()；列表<性格>敌人=新的ArrayList<>()；ArrayList<>()；列表<性格>敌人=新的ArrayList<>()；ArrayList<>()；列表<性格>敌人=新的ArrayList<>()；ArrayList<>()；列表<性格>敌人=新的ArrayList<>()；ArrayList<>()；列表<性格>敌人=新的ArrayList<>()；ArrayList<>()；列表<性格>敌人=新的ArrayList<>()；ArrayList<>()；列表<性格>敌人=新的ArrayList<>()；
+新的性格("demy_1"，"森林狼"，30，5，1，性格.类型.敌人)；添加(新的性格("敌人_1"，"森林狼"，30，5，1，字符.类型.敌人))；("demy_1"，"森林狼"，30，5，1，性格.类型.敌人)；添加(新的性格("敌人_1"，"森林狼"，30，5，1，字符.类型.敌人))；("demy_1"，"森林狼"，30，5，1，性格.类型.敌人)；添加(新的性格("敌人_1"，"森林狼"，30，5，1，字符.类型.敌人))；("demy_1"，"森林狼"，30，5，1，性格.类型.敌人)；添加(新的性格("敌人_1"，"森林狼"，30，5，1，字符.类型.敌人))；
+        
+列表<项目>奖励=新ArrayList<>()；列表<项>奖励=新的ArrayList<>()；ArrayList<>()；列表<项>奖励=新的ArrayList<>()；ArrayList<>()；列表<项>奖励=新的ArrayList<>()；ArrayList<>()；列表<项>奖励=新的ArrayList<>()；
+新的项("sword_1"，"铁剑"，"攻击力+3"，项.类型.设备，3，0，0))；添加(新的项("sword_1"，"铁剑"，"攻击力+3"，项目类型设备，3，0，0))；("sword_1"，"铁剑"，"攻击力+3"，项.类型.设备，3，0，0))；添加(新的项("sword_1"，"铁剑"，"攻击力+3"，项目类型设备，3，0，0))；("sword_1"，"铁剑"，"攻击力+3"，项.类型.设备，3，0，0))；添加(新的项("sword_1"，"铁剑"，"攻击力+3"，项目类型设备，3，0，0))；("sword_1"，"铁剑"，"攻击力+3"，项.类型.设备，3，0，0))；添加(新的项("sword_1"，"铁剑"，"攻击力+3"，项目类型设备，3，0，0))；
+        
+返回新级别(身份标识，"战斗关卡"，敌人，奖励)；返回新级别(身份标识，"战斗关卡"，敌人，奖励)；(身份标识，"战斗关卡"，敌人，奖励)； 返回新级别(身份标识，"战斗关卡"，敌人，奖励)；(身份标识，"战斗关卡"，敌人，奖励)；返回新级别(身份标识，"战斗关卡"，敌人，奖励)； (身份标识，"战斗关卡"，敌人，奖励)；返回新级别(身份标识，"战斗关卡"，敌人，奖励)；
+}}
+    
+// 辅助方法：创建示例事件// 辅助方法：创建示例事件
+私有事件createSampleEvent(字符串ID){私人的事件createSampleEvent(线身份标识){createSampleEvent(字符串身份标识){私人的事件createSampleEvent(线身份标识){createSampleEvent(字符串ID){私人的事件createSampleEvent(线身份标识){createSampleEvent(字符串身份标识){私人的事件createSampleEvent(线身份标识){
+返回新事件(身份标识，”神秘祭坛"，"你发现了一个古老的祭坛，上面刻着奇怪的符文"，返回 新的 事件(身份标识，"神秘祭坛", "你发现了一个古老的祭坛， 上面刻着奇怪的符文",(身份标识，”神秘祭坛"，"你发现了一个古老的祭坛，上面刻着奇怪的符文"，返回 新的 事件(身份标识，"神秘祭坛", "你发现了一个古老的祭坛，上面刻着奇怪的符文",(身份标识，”神秘祭坛"，"你发现了一个古老的祭坛，上面刻着奇怪的符文"，返回 新的 事件(身份标识， "神秘祭坛", "你发现了一个古老的祭坛， 上面刻着奇怪的符文",(身份标识，”神秘祭坛"，"你发现了一个古老的祭坛，上面刻着奇怪的符文"， 返回 新的 事件(身份标识，"神秘祭坛", "你发现了一个古老的祭坛，上面刻着奇怪的符文",
+列表.(…… 的((…… 的((…… 的((…… 的(
+新建事件.选项("触摸祭坛"，"你感到一股力量涌入体内"，新的 事件.选项("触摸祭坛", "你感到一股力量涌入体内",("触摸祭坛"， "你感到一股力量涌入体内"， 新的 事件.选项("触摸祭坛", "你感到一股力量涌入体内",("触摸祭坛"，"你感到一股力量涌入体内"， 新的 事件.选项("触摸祭坛", "你感到一股力量涌入体内",("触摸祭坛"，"你感到一股力量涌入体内"， 新的 事件.选项("触摸祭坛", "你感到一股力量涌入体内",
+无效的，0，2，0)，无效的,0, 2, 0),0，2，0)，无效的,0, 2, 0),0，2，0)，无效的,0, 2, 0),0，2，0)，无效的,0, 2, 0),
+新建事件.选项("摧毁祭坛"，"祭坛破碎，从中掉出了一件物品"，新的 事件.选项("摧毁祭坛", "祭坛破碎，从中掉出了一件物品",("摧毁祭坛"， "祭坛破碎，从中掉出了一件物品"，新的 事件.选项("摧毁祭坛", "祭坛破碎，从中掉出了一件物品",("摧毁祭坛"，"祭坛破碎，从中掉出了一件物品"， 新的 事件.选项("摧毁祭坛", "祭坛破碎，从中掉出了一件物品",("摧毁祭坛"， "祭坛破碎，从中掉出了一件物品"，新的 事件.选项("摧毁祭坛", "祭坛破碎，从中掉出了一件物品",
+新的项("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，("potion_1"，"治疗药水"，"恢复10点生命"，新的项("potion_1"，"治疗药水"，"恢复10点生命"，
+项目类型耗材，
+0000000000000000，
+))))))));));));));));
+}}
+    
+//Getter方法//吸气剂方法
+公共rentMap(){LevelMap getCurrentMap(){公共LevelMap getCurrentMap(){LevelMap getCurrentMap(){公共LevelMap getCurrentMap(){LevelMap getCurrentMap(){公共LevelMap getCurrentMap(){LevelMap getCurrentMap(){公共LevelMap getCurrentMap(){LevelMap getCurrentMap(){公共LevelMap getCurrentMap(){LevelMap getCurrentMap(){公共LevelMap getCurrentMap(){
+返回电流映射；返回currentMap；
+}}
+    
+公共节点getCurrentNode(()getCurrentNode(){){公共节点getCurrentNode(){
+返回电流节点；返回currentNode；
+}}
+                                                                                                                                                                                                                                                                           }
